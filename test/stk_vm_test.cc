6da5368110248763fe6488371aa10d434470c95d
forsilence <3312034934@qq.com>
2022-12-29 12:48:08 +0800

    frame stuff

--- test/stk_vm_test.cc
+++ test/stk_vm_test.cc
@@ -7,6 +7,21 @@ typedef long long Vm_int;
 typedef double Vm_double;
 typedef unsigned char Vm_char;
 typedef bool Vm_bool;
+typedef uint8_t CodeUnit;
+
+Vm_int sign_extend(uint16_t v){
+  if((v >> 15) == 1){
+    v |= 0xFFFFFFFFFFFF0000;
+  }
+  return v;
+};
+
+class FrameState;
+
+class FuncProto {
+  CodeUnit *codes;
+  friend FrameState;
+};
 
 typedef union {
   void *obj;
@@ -19,6 +34,7 @@ typedef union {
 typedef Value* TStk;
 
 class Frame{
+  protected:
   TStk stk_base;
   TStk stk_top;
   Value *locals;
@@ -39,11 +55,54 @@ class Frame{
     }
 
     void pushi(Vm_int v){
-      stkp->bv = v;
+      stkp->iv = v;
       stkp++;
     }
 
+    void pushd(Vm_double v){
+      stkp->dv = v;
+      stkp++;
+    }
+
+    Vm_int popi(){
+      stkp--;
+      return stkp->iv;
+    }
+
 };
+
+
+typedef enum {
+  ipush = 0x01, // push 2byte as Vm_int
+  iadd = 0x02, // add two top int value
+} Instr;
+
+
+class FrameState{
+  Frame *frame;
+  FuncProto *proto;
+  FrameState *parent;
+  public:
+    typedef CodeUnit* TPc;
+  private:
+    TPc pc;
+
+    Vm_int iread(){
+      return sign_extend((*(pc++)) << 4 | *(pc++));
+    }
+
+    CodeUnit instr_read(){
+      return *(pc++);
+    }
+
+};
+
+class VmRuntimeContext {
+  FrameState *main;
+  FrameState *active_frame;
+};
+
+// void dispatch(Frame *frame, )
 
 int main(int argc, char const *argv[])
 {
