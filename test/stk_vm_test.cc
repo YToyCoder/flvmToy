9b5f16467cafdb23e48f237aba0486e9eea66327
forsilence <3312034934@qq.com>
2022-12-29 14:26:50 +0800

    frame instruction dispatch

--- test/stk_vm_test.cc
+++ test/stk_vm_test.cc
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string>
 // tests for a stack vm
 
 typedef long long Vm_int;
@@ -20,7 +21,12 @@ class FrameState;
 
 class FuncProto {
   CodeUnit *codes;
+  size_t code_size;
   friend FrameState;
+  public:
+    size_t c_size(){
+      return code_size;
+    }
 };
 
 typedef union {
@@ -78,7 +84,13 @@ typedef enum {
 } Instr;
 
 
-class FrameState: Frame{
+class FrameInstrDisptcher {
+  public:
+  virtual void dispatch(CodeUnit instr) = 0;
+  virtual FrameInstrDisptcher *renewFrame(FrameState *frame) = 0; 
+};
+
+class FrameState: public Frame{
   FuncProto *proto;
   FrameState *parent;
   public:
@@ -87,27 +99,116 @@ class FrameState: Frame{
     {
       this->proto = proto_;
     }
+
+    const char *toString(){
+      std::string ret;
+      for(TStk i=stk_base; i<stk_top; i++){
+        ret += i->iv + " ";
+      }
+      ret += "\n";
+      for(int i=0; i<max_locals_size; i++){
+        ret += locals[i].iv + " ";
+      }
+      ret += "\n";
+      return ret.c_str();
+    }
+
+    void setParent(FrameState *frame){
+      parent = frame;
+    }
   private:
     TPc pc;
 
+  public:
     Vm_int iread(){
-      return sign_extend((*(pc++)) << 4 | *(pc++));
+      return sign_extend((*(pc++)) << 8 | *(pc++));
     }
 
     CodeUnit instr_read(){
       return *(pc++);
     }
 
+    bool stop(){
+      return pc < (proto->codes + proto->c_size());
+    }
+
+    static FrameState *create(FuncProto *proto, size_t max_ss, size_t max_ls){
+      // FrameState *ret = (FrameState *) malloc(sizeof(FrameState));
+      return new FrameState(max_ss, max_ls, proto);
+    }
 };
+
 
 class VmRuntimeContext {
   FrameState *main;
   FrameState *active_frame;
+  FrameInstrDisptcher *instr_dispatcher;
+  public:
+    VmRuntimeContext(FrameInstrDisptcher *dispatcher){
+      instr_dispatcher = dispatcher;
+    }
+
+    void push_frame(FrameState *frame){
+      if(main == nullptr){
+        main = frame;
+        active_frame = frame;
+      }else {
+        frame->setParent(active_frame);
+        active_frame = frame;
+      }
+      instr_dispatcher->renewFrame(active_frame);
+    }
+
+    void run(){
+      active_frame->init();
+      while(!active_frame->stop()){
+        instr_dispatcher->dispatch(active_frame->instr_read());
+      }
+    }
 };
 
-// void dispatch(Frame *frame, )
+class VirtualInstrDispatcher:public FrameInstrDisptcher {
+  FrameState *frame;
+  public:
+  virtual VirtualInstrDispatcher *renewFrame(FrameState *frame) override 
+  {
+    frame = frame;
+    return this;
+  }
+
+  void _ipush(){
+    Vm_int v = frame->iread();
+    frame->pushi(v);
+  }
+
+  void _iadd(){
+    Vm_int v1 = frame->popi();
+    Vm_int v2 = frame->popi();
+    frame->pushi(v1 + v2);
+  }
+
+  virtual void dispatch(CodeUnit instr) override {
+    switch(instr){
+      case ipush:
+        _ipush();
+        break;
+      case iadd:
+        _iadd();
+        break;
+    };
+    printf("%s\n", frame->toString());
+  }
+};
+
+
+void run(){
+  VirtualInstrDispatcher *dispatcher = new VirtualInstrDispatcher();
+  VmRuntimeContext context(dispatcher);
+  context.push_frame(FrameState::create(nullptr, 3, 1));
+}
 
 int main(int argc, char const *argv[])
 {
+  run();
   return 0;
 }
