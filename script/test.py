6af8ed1f9e99dadc590b7914322eea033374e680
forsilence <3312034934@qq.com>
2022-12-30 00:29:37 +0800

    filter for test file

--- script/test.py
+++ script/test.py
@@ -1,5 +1,8 @@
 #!E:/python-/python
 import os
+import sys
+import re
+import itertools
 
 dir,file = os.path.split(os.path.abspath(__file__))
 root = os.path.dirname(dir)
@@ -26,6 +29,17 @@ def build_file(file):
   cmd = f"{cc} {source} -o {target}"
   log(f"execute cmd : {cmd}")
   os.system(cmd)
+
+if len(sys.argv) > 1:
+  # testFiles = 
+  mstr = sys.argv[1]
+  temp = []
+  for f in testFiles:
+    if re.match(f".*{mstr}.*", f) != None:
+      temp.append(f)
+  testFiles = temp
+  print(f"filter len {len(testFiles)}")
+
 
 for el in testFiles:
   if el.endswith(".cc"):
