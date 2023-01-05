685bf4ca7425653e35e0ef9f0ef5e6641db7307b
forsilence <3312034934@qq.com>
2023-01-05 22:59:27 +0800

    StringPool test

---
+++ test/string_pool_test.cc
@@ -0,0 +1,20 @@
+#include "../flvm.cc"
+
+FlString* ofConstString(const char *p, FlStringContsPool *pool){
+  int size = 0;
+  char *walk = const_cast<char *>(p);
+  while(*walk != '\0'){
+    size++;
+    walk++;
+  }
+  return pool->ofFlstring(p, size);
+}
+
+int main(int argc, char const *argv[])
+{
+  FlStringContsPool pool;
+  auto s1 = ofConstString("hello", &pool);
+  auto s2 = ofConstString("hello", &pool);
+  printf("%s\n", s1 == s2 ? "true" : "false");
+  return 0;
+}
