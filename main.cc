917934d29bec9de9b2d12bbc1b3871f120aca5b7
forsilence <3312034934@qq.com>
2023-01-05 17:43:42 +0800

    fix bug

--- main.cc
+++ main.cc
@@ -1,5 +1,12 @@
 #include <iostream>
 
+enum Instruction {
+  iconst_1 = 0x01,
+  iconst_2 = 0x01,
+  iconst_3 = 0x01,
+  iconst_4 = 0x01,
+};
+
 int main(int argc, char const *argv[])
 {
   std::cout << "hello, this is flvm" << std::endl;
--- test/core_test.cc
+++ test/core_test.cc
@@ -1,14 +1,8 @@
 #include <iostream>
-#include "../flvmToy.hpp"
 #include "flvmt.h"
 
 int main(int argc, char const *argv[])
 {
   /* code */
-  flvm::core::FlStack<int> istk(10);
-  assert_true(istk.isEmpty(), "init stack should be empty");
-  istk.push(10);
-  assert_true(istk.pop() == 10, "10");
-  assert_true(istk.isEmpty(), "stack should be empty");
   return 0;
 }
