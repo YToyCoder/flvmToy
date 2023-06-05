9a9be1124ab6ab946efe100e2343cd63f72265a2
forsilence <3312034934@qq.com>
2023-06-05 22:38:56 +0800

    test

--- flvmToy/main.cc
+++ flvmToy/main.cc
@@ -1,11 +1,22 @@
 #include "flvm.hpp"
 #include "unicode/brkiter.h"
 #include "token.h"
+#ifdef FLVM_TEST
 
-int main(int argc, char** argv) {
+void do_test()
+{
 #ifdef TOK_TEST
 	tok_test1();
 #endif
+}
+
+#define DO_TEST do_test()
+#else 
+#define DO_TEST 
+#endif
+
+int main(int argc, char** argv) {
+	DO_TEST;
 	printf("flvm-toy-main\n");
 	return 0;
 }
