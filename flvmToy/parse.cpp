338787fbafdee93001b393fa6180418d1bbcf257
forsilence <3312034934@qq.com>
2023-06-08 22:33:16 +0800

    parser fetch token

--- flvmToy/parse.cpp
+++ flvmToy/parse.cpp
@@ -1,6 +1,39 @@
 #include "parse.h"
 #include "lex.h"
 
+/* tokens */
+void Parser::try_fill_cache()
+{
+	if (_m_tok_cache.empty() && _m_lex.has_token())
+	{
+		for (int i = 0; i < Parser_Token_Cache_Limit; i++)
+		{
+			if (!_m_lex.has_token()) break;
+			_m_tok_cache.push_back(_m_lex.next_token());
+		}
+	}
+}
+
+token_t Parser::token()
+{
+	try_fill_cache();
+	return _m_tok_cache.empty() ? InvalidTok : _m_tok_cache.front();
+}
+
+token_t Parser::next_tok()
+{
+	try_fill_cache();
+	if (_m_tok_cache.empty())
+		return InvalidTok; 
+	token_t tok = _m_tok_cache.front();
+	_m_tok_cache.pop_front();
+	return tok;
+}
+
+bool Parser::has_tok()
+{
+	return !_m_tok_cache.empty() || _m_lex.has_token();
+}
 
 IRNode* Parser::parse()
 {
@@ -30,3 +63,10 @@ IRNode* Parser::parsing_id()
 	token_t t = _m_lex.next_token();
 	return new IR_Id(t, tok_foffset(t), tok_end(t));
 }
+
+//IRNode* Parser::parsing_literal()
+//{
+//}
+//IRNode* Parser::parsing_binary()
+//{
+//}
--- flvmToy/parse.h
+++ flvmToy/parse.h
@@ -1,15 +1,29 @@
 #pragma once
 #include "lex.h"
 #include "ir.h"
+#include <list>
+
+#define Parser_Token_Cache_Limit 2
 
 class Parser
 {
 public:
+	Parser(const std::string& filename):_m_lex(filename){}
+	Parser(): Parser("") {}
 	IRNode* parse();
 protected:
 	IRNode* parsing_num();
 	IRNode* parsing_id();
+	//IRNode* parsing_binary();
+	//IRNode* parsing_literal();
+
+	//				token				//
+	token_t token(); 
+	token_t next_tok();
+	bool		has_tok(); 
+	void		try_fill_cache();
 private:
+	std::list<token_t> _m_tok_cache;
 	Lex _m_lex;
 };
 
