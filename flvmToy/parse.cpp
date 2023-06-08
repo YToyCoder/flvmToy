6f3aa59becbf68a53258bbd9a83af61db63dd75c
forsilence <3312034934@qq.com>
2023-06-08 20:54:46 +0800

    number node get num value

--- flvmToy/parse.cpp
+++ flvmToy/parse.cpp
@@ -1,4 +1,5 @@
 #include "parse.h"
+#include "lex.h"
 
 
 IRNode* Parser::parse()
@@ -10,7 +11,17 @@ IRNode* Parser::parsing_num()
 {
 	token_t t = _m_lex.next_token();
 	IR_Num* node = new IR_Num(t, tok_foffset(t), tok_end(t));
-	node->_m_num;
+	Lex::UStr str = _m_lex.token_string(t);
+	std::string u8s;
+	str.toUTF8String(u8s);
+	switch (token_kind(t)) {
+	case TokInt:
+		node->_m_num.i_value = std::stoi(u8s);
+		break;
+	case TokFloat:
+		node->_m_num.db_value= std::stod(u8s);
+		break;
+	}
 	return node;
 }
 
