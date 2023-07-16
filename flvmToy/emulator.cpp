70c92244d3ea3ff703f61772098dafa421600833
YToyCoder <3312034934@qq.com>
2023-07-16 10:43:38 +0800

    emulator

---
+++ flvmToy/emulator.cpp
@@ -0,0 +1,65 @@
+#include "emulator.h"
+
+void Emulator::dispatch_instruction(instr_t instr) {
+  switch(instr) {
+    case Instruction::nop:                  break;
+    case Instruction::iconst_0: iconst_0(); break;
+    case Instruction::iconst_1: iconst_1(); break;
+    case Instruction::iconst_2: iconst_2(); break;
+    case Instruction::iconst_3: iconst_3(); break;
+    case Instruction::iconst_4: iconst_4(); break;
+    case Instruction::dconst_1: dconst_1(); break;
+    case Instruction::dconst_2: dconst_2(); break;
+    case Instruction::ipush:    pushi();    break;
+    case Instruction::dpush:    pushd();    break;
+    case Instruction::iload:    loadi();    break;
+    case Instruction::dload:    loadd();    break;
+    case Instruction::istore:   storei();   break;
+    case Instruction::dstore:   stored();   break;
+    case Instruction::bstore:   storeb();   break;
+    case Instruction::sstore:   stores();   break;
+    case Instruction::iadd:     addi();     break;
+    case Instruction::dadd:     addd();     break;
+    case Instruction::dsub:     subd();     break;
+    case Instruction::isub:     subi();     break;
+    case Instruction::imul:     muli();     break;
+    case Instruction::dmul:     muld();     break;
+    case Instruction::idiv:     divi();     break;
+    case Instruction::ddiv:     divd();     break;
+    case Instruction::ldci:     ldci();     break;
+    case Instruction::ldcd:     ldcd();     break;
+    case Instruction::ldcs:     ldcs();     break;
+    case Instruction::i2d:      i2d();      break;
+    case Instruction::i2b:      i2b();      break;
+    case Instruction::d2i:      d2i();      break;
+// ******************** int cmp **********************
+#define CaseIntergalCmp(instruction, op) \
+  case Instruction::## instruction ##: { \
+    FlInt top_int = popi();              \
+    uint8_t jmp_offset = read_instr();   \
+    if(top_int op 0) set_pc(jmp_offset); \
+  }                                      \
+  break
+// ***************************************************
+
+    CaseIntergalCmp(ifeq, ==);
+    CaseIntergalCmp(iflt, <);
+    CaseIntergalCmp(ifle, <=);
+    CaseIntergalCmp(ifgt, >);
+    CaseIntergalCmp(ifge, >=);
+    case Instruction::go: set_pc(read_instr()); break;
+    case Instruction::dcmp: {
+      FlDouble d1 = popd();
+      FlDouble d2 = popd();
+      FlDouble sub = d2 - d1;
+      if(sub == 0) topFrame->pushi(0);
+      else if(sub < 0) topFrame->pushi(-1);
+      else topFrame->pushi(1);
+    }
+    break;
+
+    default:
+      printf("not support instruction : %s \n", instr_name((Instruction::Code)instr));
+      throw std::exception(("not support instruction : " + std::to_string(instr)).c_str());
+  };
+}
--- flvmToy/flvm.hpp
+++ flvmToy/flvm.hpp
@@ -4,7 +4,6 @@
 #include <stdint.h>
 
 #include "FlValue.h"
-#include "Fn.h"
 
 #if defined(WIN32) || defined(_WIN32)
 #include <windows.h>
@@ -256,90 +255,4 @@ private:
   FlTagValue* k_cache;
   size_t    k_cap;
   size_t    k_len;
-};
-
-class ExecFrame {
-public:
-  void pushd(FlDouble v){ stkp->set(v); stkp++; }
-  void pushb(FlBool v)  { stkp->set(v); stkp++; }
-  void pushi(FlInt v)   { stkp->set(v); stkp++; }
-  template<typename _Ty>
-  void pushv(_Ty v) { stkp->set(v); stkp++; }
-  inline FlInt    read_locali(size_t loc) {  return locals[loc]._int(); }
-  inline FlDouble read_locald(size_t loc) {  return locals[loc]._double(); }
-public:
-  ExecFrame* next;
-  ExecFrame* pre;
-
-  FnProto* proto;
-  instr_t* pc; // pointer to code
-  FlTagValue* locals; 
-  FlTagValue* k;
-
-  FlTagValue* stkb; // base
-  FlTagValue* stkt; // top
-  FlTagValue* stkp; // pointer
-};
-
-// emulating a virtual machine
-class Emulator {
-private:
-  void dispatch_instruction(instr_t instr) ;
-// frame operations
-  inline instr_t read_instr(){ return *(topFrame->pc++);}
-  inline FlInt    popi() { return (topFrame->stkp--)->_int(); }
-  inline FlDouble popd() { return (topFrame->stkp--)->_double(); }
-  inline FlBool   popb() { return (topFrame->stkp--)->_bool(); }
-  inline FlString* pops() { return (topFrame->stkp--)->_str(); }
-  inline FlTagValue ldconst(instr_t loc) { return topFrame->k[loc]; }
-  inline void set_pc(uint16_t offset) { topFrame->pc = topFrame->proto->code + offset; }
-
-// instruction impl
-  inline void iconst_0() { topFrame->pushi(0); }
-  inline void iconst_1() { topFrame->pushi(1); }
-  inline void iconst_2() { topFrame->pushi(2); }
-  inline void iconst_3() { topFrame->pushi(3); }
-  inline void iconst_4() { topFrame->pushi(4); }
-  inline void dconst_1() { topFrame->pushd(1.0); }
-  inline void dconst_2() { topFrame->pushd(2.0); }
-  inline void pushi() { 
-    FlInt intValue = sign_extend(read_instr() | read_instr() << 8);
-    topFrame->pushi(intValue);
-  }
-  inline void pushd() { topFrame->pushd(read_instr()); }
-  inline void loadi() { topFrame->pushi(topFrame->read_locali(read_instr())); }
-  inline void loadd() { topFrame->pushd(topFrame->read_locald(read_instr())); }
-  inline void storei(){ topFrame->locals[read_instr()].set(popi()); }
-  inline void stored(){ topFrame->locals[read_instr()].set(popd()); }
-  inline void storeb(){ topFrame->locals[read_instr()].set(popb()); }
-  inline void stores(){ topFrame->locals[read_instr()].set(pops()); }
-  inline void addi()  { topFrame->pushi(popi() + popi()); }
-  inline void addd()  { topFrame->pushd(popd() + popd()); }
-  inline void subi()  { 
-    FlInt topI = popi();
-    topFrame->pushi(popi() - topI); 
-  }
-  inline void subd()  { 
-    FlDouble topD = popd();
-    topFrame->pushd(popd() - topD); 
-  }
-  inline void muli()  { topFrame->pushi(popi() * popi()); }
-  inline void muld()  { topFrame->pushd(popd() * popd()); }
-  inline void divi()  { 
-    FlInt topI = popi();
-    topFrame->pushi(popi() / topI); 
-  }
-  inline void divd()  { 
-    FlDouble topD = popd();
-    topFrame->pushd(popd() / topD); 
-  }
-
-  inline void ldci() { topFrame->pushv(ldconst(read_instr())._int()); }
-  inline void ldcd() { topFrame->pushv(ldconst(read_instr())._double()); }
-  inline void ldcs() { topFrame->pushv(ldconst(read_instr())._str()); }
-  inline void i2d()  { topFrame->pushv((FlDouble)popi()); }
-  inline void i2b()  { topFrame->pushv((FlBool)popi()); }
-  inline void d2i()  { topFrame->pushv((FlInt)popd()); }
-private:
-  ExecFrame* topFrame;
 };
